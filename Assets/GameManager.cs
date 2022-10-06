d7e9064a3bad0a965cdfd54c3bf48ce7c84f57d0
dabeastt <arjeljabuel@gmail.com>
2022-10-06 10:11:54 +0800

    Added coin UI, timer UI, win/lose conditions

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public Text coinText;
+    private int coinCount;
+    public int totalCoin;
+
+    public Text timeText;
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void AddCoin()
+    {
+        coinCount++;
+        coinText.text = "Coins Collected: " + coinCount;
+    }
+    public void CheckWinCondition()
+    {
+        if(coinCount >= totalCoin)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+    }
+    public void LoseGame()
+    {
+        SceneManager.LoadScene("GameLose");
+    }
+    public void Timer()
+    {
+        timer -= 1 * Time.deltaTime;
+        timeText.text = "Time Left: " + timer;
+    }
+    public void TimeLose()
+    {
+        if(timer <= 0f)
+        {
+            SceneManager.LoadScene("GameLose");
+        }
     }
 }
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameManager.instance.CheckWinCondition();
+        GameManager.instance.Timer();
+        GameManager.instance.TimeLose();
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            GameManager.instance.AddCoin();
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.layer == 4)
+        {
+            GameManager.instance.LoseGame();
+        }
+    }
+}
